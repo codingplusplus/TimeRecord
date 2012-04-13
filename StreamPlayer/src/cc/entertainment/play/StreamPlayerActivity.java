77abaa1e32b277098e4d0d26d6a8fcffa6fd7f46
cc191954 <cc191954@gmail.com>
2012-04-13 18:54:13 +0800

    init

---
+++ StreamPlayer/src/cc/entertainment/play/StreamPlayerActivity.java
@@ -0,0 +1,13 @@
+package cc.entertainment.play;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StreamPlayerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
